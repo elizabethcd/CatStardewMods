87ca3468d966379216add51b305b1afa1fcb5a6f
elizabethcd <55162529+elizabethcd@users.noreply.github.com>
2023-07-01 16:57:01 -1000

    Attempt at GMCM for Better Garden Pots

--- BetterGardenPots/BetterGardenPotsMod.cs
+++ BetterGardenPots/BetterGardenPotsMod.cs
@@ -6,6 +6,7 @@ using HarmonyLib;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
 using StardewValley;
 using StardewValley.Objects;
 using System;
@@ -18,17 +19,52 @@ namespace BetterGardenPots
     public class BetterGardenPotsMod : Mod
     {
         private readonly IList<IEventSubscriber> subscribers = new List<IEventSubscriber>();
+        private BetterGardenPotsModConfig Config;
+        private Harmony StoredHarmony;
 
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
             var harmony = new Harmony("cat.bettergardenpots");
+            StoredHarmony = harmony;
 
-            BetterGardenPotsModConfig config = helper.ReadConfig<BetterGardenPotsModConfig>();
+            Config = helper.ReadConfig<BetterGardenPotsModConfig>();
 
-            if (config.MakeSprinklersWaterGardenPots) this.subscribers.Add(new GardenPotSprinklerHandler(this.Helper));
+            helper.Events.GameLoop.ReturnedToTitle += this.OnReturnedToTitle;
+            helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
 
+            // Set up GMCM config when game is launched
+            helper.Events.GameLoop.GameLaunched += SetUpConfig;
+        }
+
+        /// <summary>Raised after the player loads a save slot.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void OnSaveLoaded(object sender, EventArgs e)
+        {
+            if (Config.MakeSprinklersWaterGardenPots) this.subscribers.Add(new GardenPotSprinklerHandler(this.Helper));
+
+            ApplyPatches(StoredHarmony, Config);
+
+            if (Context.IsMainPlayer)
+            {
+                foreach (IEventSubscriber subscriber in this.subscribers)
+                    subscriber.Subscribe();
+            }
+        }
+
+        /// <summary>Raised after the game returns to the title screen.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void OnReturnedToTitle(object sender, ReturnedToTitleEventArgs e)
+        {
+            foreach (IEventSubscriber subscriber in this.subscribers)
+                subscriber.Unsubscribe();
+        }
+
+        private void ApplyPatches(Harmony harmony, BetterGardenPotsModConfig config)
+        {
             Type indoorPotType = typeof(IndoorPot);
 
             IList<Tuple<string, Type, Type>> replacements = new List<Tuple<string, Type, Type>>();
@@ -63,30 +99,65 @@ namespace BetterGardenPots
                     prefix: new HarmonyMethod(typeof(FindCloseFlowerPatch), nameof(FindCloseFlowerPatch.Prefix))
                 );
             }
-
-            helper.Events.GameLoop.ReturnedToTitle += this.OnReturnedToTitle;
-            helper.Events.GameLoop.SaveLoaded += this.OnSaveLoaded;
         }
 
-        /// <summary>Raised after the player loads a save slot.</summary>
+        /// <summary>Raised when the game is launched in order to set up the GMCM config.</summary>
         /// <param name="sender">The event sender.</param>
         /// <param name="e">The event arguments.</param>
-        private void OnSaveLoaded(object sender, EventArgs e)
+        private void SetUpConfig(object sender, GameLaunchedEventArgs e)
         {
-            if (Context.IsMainPlayer)
+            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
             {
-                foreach (IEventSubscriber subscriber in this.subscribers)
-                    subscriber.Subscribe();
+                return;
             }
-        }
 
-        /// <summary>Raised after the game returns to the title screen.</summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event arguments.</param>
-        private void OnReturnedToTitle(object sender, ReturnedToTitleEventArgs e)
-        {
-            foreach (IEventSubscriber subscriber in this.subscribers)
-                subscriber.Unsubscribe();
+            // Register with GMCM
+            configMenu.Register(
+                mod: this.ModManifest,
+                reset: () => this.Config = new BetterGardenPotsModConfig(),
+                save: () => this.Helper.WriteConfig(this.Config),
+                titleScreenOnly: true);
+
+            foreach (System.Reflection.PropertyInfo property in typeof(BetterGardenPotsModConfig).GetProperties())
+            {
+                if (property.PropertyType.Equals(typeof(bool)))
+                {
+                    configMenu.AddBoolOption(
+                        mod: ModManifest,
+                        getValue: () => (bool)property.GetValue(Config),
+                        setValue: value => property.SetValue(Config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title")
+                       );
+                }
+                if (property.PropertyType.Equals(typeof(int)))
+                {
+                    configMenu.AddNumberOption(
+                        mod: ModManifest,
+                        getValue: () => (int)property.GetValue(Config),
+                        setValue: value => property.SetValue(Config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title")
+                       );
+                }
+                if (property.PropertyType.Equals(typeof(KeybindList)))
+                {
+                    configMenu.AddKeybindList(
+                        mod: ModManifest,
+                        getValue: () => (KeybindList)property.GetValue(Config),
+                        setValue: value => property.SetValue(Config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title")
+                        );
+                }
+                if (property.PropertyType.Equals(typeof(SButton)))
+                {
+                    configMenu.AddKeybind(
+                        mod: ModManifest,
+                        getValue: () => (SButton)property.GetValue(Config),
+                        setValue: value => property.SetValue(Config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title")
+                        );
+                }
+            }
         }
     }
 }
