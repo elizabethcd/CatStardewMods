0103b2b306302f2f30c5695456208fddc67b8dd5
elizabethcd <55162529+elizabethcd@users.noreply.github.com>
2023-07-02 16:26:35 -0700

    Giant Crop Ring GMCM

--- GiantCropRing/ModEntry.cs
+++ GiantCropRing/ModEntry.cs
@@ -2,6 +2,7 @@
 using Microsoft.Xna.Framework.Graphics;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
 using StardewValley;
 using StardewValley.Menus;
 using StardewValley.TerrainFeatures;
@@ -47,6 +48,9 @@ namespace GiantCropRing
             helper.Events.Content.AssetRequested += this.OnAssetRequested;
             this.config = helper.ReadConfig<ModConfig>();
             this.modHelper = helper;
+
+            // Set up GMCM config when game is launched
+            helper.Events.GameLoop.GameLaunched += SetUpConfig;
         }
 
         /// <summary>Raised after the game is launched.</summary>
@@ -162,6 +166,91 @@ namespace GiantCropRing
             this.numberOfTimeTicksWearingOneRing = 0;
             this.numberOfTimeTicksWearingTwoRings = 0;
             this.totalNumberOfSeenTimeTicks = 0;
+        }
+
+        /// <summary>Raised when the game is launched in order to set up the GMCM config.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void SetUpConfig(object sender, GameLaunchedEventArgs e)
+        {
+            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+            {
+                return;
+            }
+
+            // Register with GMCM
+            configMenu.Register(
+                mod: this.ModManifest,
+                reset: () => this.config = new ModConfig(),
+                save: () => this.Helper.WriteConfig(this.config));
+
+            foreach (System.Reflection.PropertyInfo property in typeof(ModConfig).GetProperties())
+            {
+                if (property.PropertyType.Equals(typeof(bool)))
+                {
+                    configMenu.AddBoolOption(
+                        mod: ModManifest,
+                        getValue: () => (bool)property.GetValue(this.config),
+                        setValue: value => property.SetValue(this.config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title")
+                       );
+                }
+                if (property.PropertyType.Equals(typeof(int)))
+                {
+                    configMenu.AddNumberOption(
+                        mod: ModManifest,
+                        getValue: () => (int)property.GetValue(this.config),
+                        setValue: value => property.SetValue(this.config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title"),
+                        min: 0
+                       );
+                }
+                if (property.PropertyType.Equals(typeof(double)))
+                {
+                    configMenu.AddNumberOption(
+                        mod: ModManifest,
+                        getValue: () => (float)property.GetValue(this.config),
+                        setValue: value => property.SetValue(this.config, (double)value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title"),
+                        tooltip: null,
+                        min: 0f,
+                        max: 1f,
+                        interval: 0.01f
+                       );
+                }
+                if (property.PropertyType.Equals(typeof(float)))
+                {
+                    configMenu.AddNumberOption(
+                        mod: ModManifest,
+                        getValue: () => (float)property.GetValue(this.config),
+                        setValue: value => property.SetValue(this.config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title"),
+                        tooltip: null,
+                        min: 0f,
+                        max: 1f,
+                        interval: 0.01f
+                       );
+                }
+                if (property.PropertyType.Equals(typeof(KeybindList)))
+                {
+                    configMenu.AddKeybindList(
+                        mod: ModManifest,
+                        getValue: () => (KeybindList)property.GetValue(this.config),
+                        setValue: value => property.SetValue(this.config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title")
+                        );
+                }
+                if (property.PropertyType.Equals(typeof(SButton)))
+                {
+                    configMenu.AddKeybind(
+                        mod: ModManifest,
+                        getValue: () => (SButton)property.GetValue(this.config),
+                        setValue: value => property.SetValue(this.config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title")
+                        );
+                }
+            }
         }
 
         private void MaybeChangeCrops(double chance, GameLocation environment)
