eda56f3cf5fb12671315fc90b62af87207c3b57d
elizabethcd <55162529+elizabethcd@users.noreply.github.com>
2023-07-01 17:58:53 -1000

    Custom Transparency GMCM

--- CustomTransparency/ModEntry.cs
+++ CustomTransparency/ModEntry.cs
@@ -1,5 +1,7 @@
 ﻿using HarmonyLib;
 using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using StardewModdingAPI.Utilities;
 using System.Reflection;
 
 namespace CustomTransparency
@@ -20,6 +22,9 @@ namespace CustomTransparency
             }
 
             instance.PatchAll(Assembly.GetExecutingAssembly());
+
+            // Set up GMCM config when game is launched
+            helper.Events.GameLoop.GameLaunched += SetUpConfig;
         }
 
         /// <summary>Validate the given config, resulting in a new config with values changed to the default if invalid.</summary>
@@ -38,6 +43,90 @@ namespace CustomTransparency
             else changed = true;
 
             return changed;
+        }
+
+        /// <summary>Raised when the game is launched in order to set up the GMCM config.</summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event arguments.</param>
+        private void SetUpConfig(object sender, GameLaunchedEventArgs e)
+        {
+            var configMenu = Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+            {
+                return;
+            }
+
+            // Register with GMCM
+            configMenu.Register(
+                mod: this.ModManifest,
+                reset: () => Config = new ModConfig(),
+                save: () => this.Helper.WriteConfig(Config));
+
+            foreach (System.Reflection.PropertyInfo property in typeof(ModConfig).GetProperties())
+            {
+                if (property.PropertyType.Equals(typeof(bool)))
+                {
+                    configMenu.AddBoolOption(
+                        mod: ModManifest,
+                        getValue: () => (bool)property.GetValue(Config),
+                        setValue: value => property.SetValue(Config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title")
+                       );
+                }
+                if (property.PropertyType.Equals(typeof(int)))
+                {
+                    configMenu.AddNumberOption(
+                        mod: ModManifest,
+                        getValue: () => (int)property.GetValue(Config),
+                        setValue: value => property.SetValue(Config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title")
+                       );
+                }
+                if (property.PropertyType.Equals(typeof(double)))
+                {
+                    configMenu.AddNumberOption(
+                        mod: ModManifest,
+                        getValue: () => (float)property.GetValue(Config),
+                        setValue: value => property.SetValue(Config, (double)value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title"),
+                        tooltip: null,
+                        min: 0f,
+                        max: 1f,
+                        interval: 0.01f
+                       );
+                }
+                if (property.PropertyType.Equals(typeof(float)))
+                {
+                    configMenu.AddNumberOption(
+                        mod: ModManifest,
+                        getValue: () => (float)property.GetValue(Config),
+                        setValue: value => property.SetValue(Config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title"),
+                        tooltip: null,
+                        min: 0f,
+                        max: 1f,
+                        interval: 0.01f
+                       );
+                }
+                if (property.PropertyType.Equals(typeof(KeybindList)))
+                {
+                    configMenu.AddKeybindList(
+                        mod: ModManifest,
+                        getValue: () => (KeybindList)property.GetValue(Config),
+                        setValue: value => property.SetValue(Config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title")
+                        );
+                }
+                if (property.PropertyType.Equals(typeof(SButton)))
+                {
+                    configMenu.AddKeybind(
+                        mod: ModManifest,
+                        getValue: () => (SButton)property.GetValue(Config),
+                        setValue: value => property.SetValue(Config, value),
+                        name: () => Helper.Translation.Get($"{property.Name}.title")
+                        );
+                }
+            }
         }
     }
 }
